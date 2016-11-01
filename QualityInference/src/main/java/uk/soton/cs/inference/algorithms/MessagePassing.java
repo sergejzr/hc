26839e4e7871fadb97efc53d07f2f55faa4eb2e4
Sergej Zerr <sz4d15@soton.ac.uk>
2016-11-01 13:28:20 +0000

    logs

--- QualityInference/src/main/java/uk/soton/cs/inference/algorithms/MessagePassing.java
+++ QualityInference/src/main/java/uk/soton/cs/inference/algorithms/MessagePassing.java
@@ -2,6 +2,7 @@ package uk.soton.cs.inference.algorithms;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Random;
 import java.util.Set;
@@ -34,34 +35,48 @@ public class MessagePassing {
 		
 		int k=0;
 		Date start=new Date();
+		HashSet<String> cache=new HashSet<>();
 		while (difference > delta&&k++<4) {
 			
 		
 			System.out.println("Round "+k+" start ");
 			//BigArray oldY=Y.copy();
+			Date startloop1=new Date();
+			
+			int loop1=0,loop11=0,loop12=0,loop2=0;
 			for (CSObject object : idx.getObjectindex().values()) {
 				double tmp_sum_all = 0;
-
+				Date startloop11=new Date();
 				for (Annotation annotation : object.getUsers().values()) {
 					CSUser user = annotation.getUser();
 
-					if (object.hasAnnotationExceptForUser(level,answer,user)) {
+					
+					String key=object.getId()+"|"+level+"|"+user.getId();
+					
+					if (cache.contains(key)||object.hasAnnotationExceptForUser(level,answer,user)) {
 						A.set(object.getId(), user.getId(), 1);
 						tmp_sum_all += Y.get(object.getId(), user.getId());
+						cache.add(key);
 					} else {
 						A.set(object.getId(), user.getId(), -1);
 						tmp_sum_all -= Y.get(object.getId(), user.getId());
 					}
 				}
-
+				Date d1 = new Date();
+			//	System.out.println("Loop11 took " +Math.round((d1.getTime()-startloop11.getTime())/2));
+				loop11+=d1.getTime()-startloop11.getTime();
+				Date startloop12 = d1;
 				double tmp_j = 0;
 				for (Annotation annotation : object.getUsers().values()) {
 					CSUser user = annotation.getUser();
 
-					if (object.hasAnnotationExceptForUser(level,answer,user)) {
+					String key=object.getId()+"|"+level+"|"+user.getId();
+					
+					if (cache.contains(key)||object.hasAnnotationExceptForUser(level,answer,user)) {
 						// A.set(object.getId(), user.getId(), 1);
 						// tmp_sum_all+=Y.get(object.getId(), user.getId());
 						tmp_j = Y.get(object.getId(), user.getId());
+						cache.add(key);
 					} else {
 						// A.set(object.getId(), user.getId(), -1);
 						//// tmp_sum_all-=Y.get(object.getId(), user.getId());
@@ -69,8 +84,16 @@ public class MessagePassing {
 					}
 					X.set(object.getId(), user.getId(), tmp_sum_all - tmp_j);
 				}
+				
+				Date d2 = new Date();
+				loop12+=(d2.getTime()-startloop12.getTime());
 
 			}
+			Date d2 = new Date();
+			System.out.println("Loop1 took " +Math.round((d2.getTime()-startloop1.getTime())/1000));
+			System.out.println("Loop11 took " +Math.round(loop11/1000));
+			System.out.println("Loop11 took " +Math.round(loop12/1000));
+			Date startloop2 = d2;
 			
 			for (CSObject object : idx.getObjectindex().values()) 
 			{
@@ -102,11 +125,14 @@ public class MessagePassing {
 				}
 
 			}
-			
+			Date d3= new Date();
+			System.out.println("Loop2 took " +Math.round((d3.getTime()-startloop2.getTime())/2));
+		
 			Date curdate = new Date();
 			System.out.println("Round "+k+" took "+(curdate.getTime()-start.getTime()));
 			start=curdate;
 		}
+		System.out.println("predict objects");
 		for(CSObject object:refobjects)
 		{
 			
